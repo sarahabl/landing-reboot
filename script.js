7408bcf38011da5182b673416d974b70e8fb018e
sarahabl <sarahablonet.studio@gmail.com>
2024-06-19 14:13:12 +0200

    ajout animation compteur

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ window.addEventListener('scroll', () => {
     });
 });
 
+//Animatio compteurs
+const counters = document.querySelectorAll(".number");
+
+counters.forEach((counter) => {
+    counter.innerText = "0";
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+        const increment = target / 200; // Ajustez la vitesse d'incrémentation si nécessaire
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCounter, 10); // Ajustez l'intervalle de mise à jour si nécessaire
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCounter();
+});
+
+
+
