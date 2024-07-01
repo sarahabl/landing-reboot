48f358b6e588418988ce970f2d3a1e0033f33115
sarahabl <sarahablonet.studio@gmail.com>
2024-07-01 19:27:47 +0200

    Màj hero

--- script.js
+++ script.js
@@ -2,25 +2,8 @@ window.addEventListener('scroll', () => {
     const hero = document.querySelector('.hero');
     const scrollPosition = window.scrollY;
 
-// Changez le fond et la couleur du texte en fonction de la position de défilement
-    if (scrollPosition > 100) {
-        hero.style.backgroundColor = '#892FFF'; // Changez la couleur de fond
-        hero.style.color = '#fff'; // Changez la couleur du texte
-        localStorage.color = '#F2FF46'
-    } else {
-        hero.style.backgroundColor = 'transparent';
-        hero.style.color = '#000';
-        }
 
-// Ajoutez des animations aux images
-    const images = document.querySelectorAll('.product-card img');
-        images.forEach((img, index) => {
-            img.style.transition = `transform 0.5s ease ${index * 0.1}s`;
-            img.style.transform = 'translateY(0)'; // Effet de flottement initial
-    });
-});
-
-//Animatio compteurs
+//Animation compteurs
 const counters = document.querySelectorAll(".number");
 
 counters.forEach((counter) => {
@@ -38,6 +21,3 @@ counters.forEach((counter) => {
     };
     updateCounter();
 });
-
-
-
